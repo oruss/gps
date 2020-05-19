b0d701fae1af340967b5d5d44694824f42bc0c97
Martin Emanuel Morelos Raigosa <ondalibre_12356@hotmail.com>
2020-05-18 22:29:18 -0500

    update2

--- SimGpsTransmitter.ino
+++ SimGpsTransmitter.ino
@@ -1,47 +1,39 @@
-#include <Adafruit_FONA.h>
-
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
-
+#include <stdlib.h>
+#include <I2Cdev.h>
+#include <MPU6050.h>
+#include <Wire.h>
 static String apn="em";
 static String http="http://";
 static String loggingPassword="";
 static String serverIP="gps.oruss.com.mx";
 static String IMEI="862643033176755";
-// Pins where GPS and SIM modules are connected
 static const int SimRXPin = 15, SimTXPin = 14;
 static const int GPSRXPin = 9, GPSTXPin = 8;
 static const int ErrorPin = 10, SimConnectionPin = 12;
-char rssi2;
+MPU6050 sensor;
 int DIGI1 = 4;
 int DIGI2 = 5;
 int AIN1 = A1;
 int AIN2 = A2;
-int Batteryin = A3;
-#define FONA_RST 10
-
+int ax, ay, az;
+int gx, gy, gz;
+int16_t Tmp;
+long tiempo_prev;
+float dt;
+float ang_x, ang_y;
+float ang_x_prev, ang_y_prev;
+char r;
+char n;
 static const unsigned long frequency = 60000;
-
-// Maximum time to wait SIM module for response
 static long maxResponseTime = 30000;
 String responseString;
 TinyGPSPlus gps;
 unsigned long previous=0;
 unsigned long beltTime;
-
 SoftwareSerial sim_ss(SimRXPin, SimTXPin);
 SoftwareSerial gps_ss(GPSRXPin, GPSTXPin);
-SoftwareSerial fonaSS = SoftwareSerial(SimRXPin, SimTXPin);
-SoftwareSerial *fonaSerial = &fonaSS;
-Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
-// Hardware serial is also possible!
-//  HardwareSerial *fonaSerial = &Serial1;
-
-// Use this for FONA 800 and 808s
-        uint8_t n = fona.getRSSI();
-        int8_t r;
-
-
 void setup()
 {
   int valor;
@@ -59,72 +51,50 @@ void setup()
   delay(100);
   sim_ss.begin(9600);
   Serial.begin(9600);  
-
+  Wire.begin(); 
+  sensor.initialize();   
   delay(5000); 
-  Serial.println("Iniciando... esperando hasta que el modulo se conecte a la red"); 
-  
-   sim_ss.println("AT");
-  // Wait until module is connected and ready
+  Serial.println("Iniciando... esperando hasta que el modulo se conecte a la red");  
+  sim_ss.println("AT");
   waitUntilResponse("OK");
   blinkLed(SimConnectionPin);
-
 updateSerial();
-  
-  // Full mode
   sim_ss.println("AT+CFUN=1");
   waitUntilResponse("OK");
   blinkLed(SimConnectionPin);
-
 updateSerial();
-
   sim_ss.write("AT+CSTT=\"");
   sim_ss.print(apn);
   sim_ss.write("\",\"\",\"\"\r\n");
   waitUntilResponse("OK");
   blinkLed(SimConnectionPin);
-
 updateSerial();
-
-  // Connect and get IP
   sim_ss.println("AT+CIICR");
   waitUntilResponse("OK");
   blinkLed(SimConnectionPin);
-
 updateSerial();
-
-  // Some more credentials
   sim_ss.write("AT+SAPBR=3,1,\"APN\",\"");
   sim_ss.print(apn);
   sim_ss.write("\"\r\n");
   waitUntilResponse("OK");
   blinkLed(SimConnectionPin);
-
-updateSerial();
-  
+updateSerial();  
   sim_ss.println("AT+SAPBR=3,1,\"USER\",\"\"");
   waitUntilResponse("OK");
   blinkLed(SimConnectionPin);
-
 updateSerial();
- 
   sim_ss.println("AT+SAPBR=3,1,\"PWD\",\"\"");
   waitUntilResponse("OK");
   blinkLed(SimConnectionPin);
-
 updateSerial();
-
   sim_ss.println("AT+SAPBR=1,1");
   waitUntilResponse("OK");
   blinkLed(SimConnectionPin);
-
 updateSerial();
-
   sim_ss.println("AT+HTTPINIT");
   waitUntilResponse("OK");
   digitalWrite(SimConnectionPin, HIGH);
-
-updateSerial();
-  
+updateSerial(); 
   gps_ss.listen();
   previous = millis();
   Serial.println("Iniciando Comunicación Satelital");
@@ -138,7 +108,6 @@ void blinkLed(int led)
   delay(20);
   digitalWrite(led, LOW);
 }
-
 void waitUntilResponse(String response)
 {
   beltTime = millis();
@@ -150,20 +119,19 @@ void waitUntilResponse(String response)
     totalResponse = totalResponse + responseString;
     Serial.println(responseString);
   }
-
   if(totalResponse.length() <= 0)
   {
     Serial.println("No hay respuesta del modulo. Revisar conexiones, tarjeta SIM y voltaje!");
     digitalWrite(ErrorPin, HIGH);
-    delay(3000);
+    delay(30000);
     exit(0); // No way to recover
   }
   else if (responseString.indexOf(response) < 0)
   {
-    Serial.println("Respuesta inesperada del modulo");
+    Serial.println("Unexpected response from the module");
     Serial.println(totalResponse);
     digitalWrite(ErrorPin, HIGH);
-    delay(3000);
+    delay(30000);
     exit(0); // No way to recover
   }
 }
@@ -188,25 +156,29 @@ void tryToRead()
     responseString += c; //makes the string readString
   }
 }
-
 void loop()
 {
-  // If we have data, decode and log the data
   while (gps_ss.available() > 0)
    if (gps.encode(gps_ss.read()))
     logInfo();
-
-  // Test that we have had something from GPS module within first 10 seconds
   if (millis() - previous > 10000 && gps.charsProcessed() < 10)
   {
-    Serial.println("error de coneción de GPS!");
+    Serial.println("GPS wiring error!");
     while(true);
   }
-
+   sensor.getAcceleration(&ax, &ay, &az);
+  sensor.getRotation(&gx, &gy, &gz); 
+  dt = (millis()-tiempo_prev)/1000.0;
+  tiempo_prev=millis();
+  float accel_ang_x=atan(ay/sqrt(pow(ax,2) + pow(az,2)))*(180.0/3.14);
+  float accel_ang_y=atan(-ax/sqrt(pow(ay,2) + pow(az,2)))*(180.0/3.14);
+  ang_x = 0.98*(ang_x_prev+(gx/131)*dt) + 0.02*accel_ang_x;
+  ang_y = 0.98*(ang_y_prev+(gy/131)*dt) + 0.02*accel_ang_y;
+  ang_x_prev=ang_x;
+  ang_y_prev=ang_y;
 }
 void logInfo()
-{  
-  
+{   
     uint8_t hr_val, min_val, sec_val;
     int Day_val, Month_val;
     long  Year_val, vel_val;
@@ -218,10 +190,10 @@ void logInfo()
     int value2 = 0;
     int valueA1 = 0;
     int valueA2 = 0;
-    int valueBat = 0;
     long positionA1,positionA2;
-    int positionBat;
-
+    double lectura;
+    double Vout;
+    double battery_val;
     Day_val = gps.date.day();
     Month_val = gps.date.month();
     Year_val = gps.date.year();
@@ -233,25 +205,19 @@ void logInfo()
     angle_val=0;
     rssi_val=0;
     current_val=0;
-
     value = digitalRead(DIGI1);
     value2 = digitalRead(DIGI2);
     valueA1 = analogRead(AIN1);
     valueA2 = analogRead(AIN2);
-    valueBat = analogRead(Batteryin);
+    lectura = analogRead(A3);
     positionA1 = map(valueA1, 0, 1023, 0, 5);
     positionA2 = map(valueA2, 0, 1023, 0, 5);
-    positionBat = map(valueBat, 0, 1023, 0, 4);
-  // Causes us to wait until we have satelite fix
-
-
   if(!gps.location.isValid())
   {
     Serial.println("Ubicación invalida. esperando por conexión satelital.");
     blinkLed(ErrorPin);
     return;
   }
-
 String latitud(gps.location.lat(), DEC);
 String longitud (gps.location.lng(),DEC);
 String alt_string(gps.altitude.meters(),DEC);
@@ -260,16 +226,16 @@ String digistate2(value2);
 String Vel(vel_val); 
 String angle(angle_val);
 String rssi(rssi_val);
-String bat(positionBat);
 String amps(current_val);
 String Hrs;
 String Mins;
 String Segs;
 String Month;
 String Day;
-
   if(millis() - previous > frequency)
   {
+ Vout = map(lectura,0,1023,0,400);
+ battery_val = Vout / 100.00;
     if(gps.time.hour() < 10){
        Hrs = "0";
     }else{Hrs="";}
@@ -285,21 +251,20 @@ String Day;
     if(gps.date.day() < 10){
         Day = "0";
     }else{Day="";}
-    
     sim_ss.listen();
     previous = millis();
-
- 
-        Serial.print(F("RSSI = ")); Serial.print(n); Serial.print(": ");
-        if (n == 0) r = -115;
-        if (n == 1) r = -111;
-        if (n == 31) r = -52;
-        if ((n >= 2) && (n <= 30)) {
-          r = map(n, 2, 30, -110, -54);
-        }
-        Serial.print(r); Serial.println(F(" dBm"));
-
-   
+sim_ss.println("AT+CSQ"); // SIGNAL STRENGTH
+delay(500);
+if (sim_ss.find(": ")) { // decode reply
+while (sim_ss.available()) {
+     n = sim_ss.read();
+      if (n == ',') break;
+      if (n != '\n'){sim_ss.write(n);    
+         r = n;     
+      }             
+}
+delay(400);
+}    
     String  url ="AT+HTTPPARA=\"URL\",\"";
     url += http;
     url += serverIP;
@@ -332,7 +297,7 @@ String Day;
     url += ",";
     url += vel_val;
     url += ",";
-    url += angle_val;
+    url += ang_y;
     url += ",";
     url += dig1state;
     url += ",";
@@ -342,9 +307,9 @@ String Day;
     url += ",";
     url += positionA2;
     url += ",";
-    url += n;
+    url += r;
     url += ",";
-    url += bat;
+    url +=  battery_val;
     url += ",";
     url += amps;
     url += "\"";
@@ -353,8 +318,8 @@ String Day;
     digitalWrite(SimConnectionPin, LOW);
     sim_ss.println("AT+HTTPACTION=0");
     waitUntilResponse("+HTTPACTION:");
-    digitalWrite(SimConnectionPin, HIGH); 
-    gps_ss.listen();
+    digitalWrite(SimConnectionPin, HIGH);
+    gps_ss.listen();  
   }
 }
 void updateSerial()
